90bac1d6ea75865c2acebddf04b8b54d9b89d3d7
Jenny Belanger <jbelangr@gmail.com>
2022-02-27 19:44:59 -0500

    Add 404 page

---
+++ example/src/Error404.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Error() {
+	return (
+		<p>The requested URL was not found on this server.</p>
+	);
+}
--- example/src/Routes.js
+++ example/src/Routes.js
@@ -1,5 +1,6 @@
 import { Route, Switch } from 'react-router-dom';
 import BasicForm from './Examples/BasicForm';
+import Error404 from './Error404';
 import HorizontalForm from './Examples/HorizontalForm';
 import JsonApiAdd from './Examples/JsonApiAdd';
 import JsonApiEdit from './Examples/JsonApiEdit';
@@ -14,6 +15,7 @@ export default function Routes() {
 			<Route exact path="/json-api-add" component={JsonApiAdd} />
 			<Route exact path="/json-api-edit/:id" component={JsonApiEdit} />
 			<Route exact path="/options" component={Options} />
+			<Route component={Error404} />
 		</Switch>
 	);
 }
