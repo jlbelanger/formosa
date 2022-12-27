0185f96b68a3c59a1a1f866ca2d858a4302d8d67
Jenny Belanger <jbelangr@gmail.com>
2022-12-27 10:42:35 -0500

    Fix deprecation warning

--- example/src/index.js
+++ example/src/index.js
@@ -1,5 +1,6 @@
 import App from './App';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
